c97818363e3abdec28f44d05ffa0f5fdf4eddd27
d1nd1nok <dilnaz.saduakasova.05@mail.ru>
2025-02-25 16:39:09 +0500

    login activity and layout

--- app/src/main/java/com/example/movie_app/LoginActivity.java
+++ app/src/main/java/com/example/movie_app/LoginActivity.java
@@ -1,24 +1,49 @@
 package com.example.movie_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText userEdt, passEdt;
+    private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        initView();
+
+    }
+
+    private void initView() {
+        userEdt = findViewById(R.id.editTextText);
+        passEdt = findViewById(R.id.editTextPasswors);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(v -> {
+            if(userEdt.getText().toString().isEmpty() ||
+                    passEdt.getText().toString().isEmpty()){
+
+                Toast.makeText(LoginActivity.this, "Fill username and password", Toast.LENGTH_SHORT).show();
+
+            } else if (userEdt.getText().toString().equals("test") &&
+                    passEdt.getText().toString().equals("test")) {
+
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+            }else {
+
+                Toast.makeText(LoginActivity.this, "Incorrect username and password", Toast.LENGTH_SHORT).show();
+
+            }
         });
     }
 }
